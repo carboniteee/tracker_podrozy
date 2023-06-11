8d1fa48a1785354a6034e1726ed5572416dd1f84
carboniteee <kinga.kanik@smcebi.edu.pl>
2023-06-12 00:55:27 +0200

    update ekranu z dodawaniem podróży, zmienne przygotowane do wstawienia do bazy (Dodaj.kt) v1.0

--- app/src/main/java/com/example/routpixal/Dodaj.kt
+++ app/src/main/java/com/example/routpixal/Dodaj.kt
@@ -1,26 +1,57 @@
 package com.example.routpixal
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.Button
+import android.widget.EditText
 import android.widget.LinearLayout
+import android.widget.RatingBar
 import androidx.appcompat.app.AppCompatActivity
 
 class Dodaj : AppCompatActivity() {
     private lateinit var pointsContainer: LinearLayout
-    private lateinit var buttonAddPoint: Button
+    private lateinit var btnAddPoint: Button
+    private lateinit var edRouteName: EditText
+    private lateinit var ratingBar: RatingBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.dodaj)
         pointsContainer = findViewById(R.id.pointsContainer)
-        buttonAddPoint = findViewById(R.id.buttonAddPoint)
-        buttonAddPoint.setOnClickListener {
+        btnAddPoint = findViewById(R.id.btnAddPoint)
+        edRouteName = findViewById(R.id.edRouteName)
+        ratingBar = findViewById(R.id.ratingBar)
+
+        btnAddPoint.setOnClickListener {
             addIntermediatePointField()
+        }
+
+        val btnSave = findViewById<Button>(R.id.btnSave)
+        btnSave.setOnClickListener {
+            val routeName = edRouteName.text.toString()
+            val rating = ratingBar.rating
+
+            val allPoints = mutableListOf<String>()
+
+            val startPoint = findViewById<EditText>(R.id.edStart).text.toString()
+            allPoints.add(startPoint)
+
+            for (i in 0 until pointsContainer.childCount - 1) {
+                val pointLayout = pointsContainer.getChildAt(i) as LinearLayout
+                val editTextPoint = pointLayout.findViewById<EditText>(R.id.pointsContainer)
+                val point = editTextPoint.text.toString()
+                allPoints.add(point)
+            }
+
+            val endPoint = findViewById<EditText>(R.id.edEnd).text.toString()
+            allPoints.add(endPoint)
+
+            // teraz te dane można wpisać do bazy danych
         }
     }
 
     private fun addIntermediatePointField() {
-        val pointLayout = layoutInflater.inflate(R.layout.dodatkowy_punkt, pointsContainer, false)
+        val pointLayout = LayoutInflater.from(this).inflate(R.layout.dodatkowy_punkt, pointsContainer, false)
         val buttonDeletePoint = pointLayout.findViewById<Button>(R.id.btnUsun)
         buttonDeletePoint.setOnClickListener {
             pointsContainer.removeView(pointLayout)
